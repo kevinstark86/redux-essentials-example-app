f40d2140fe6a3542ed20e588e49fe9d2de5f2f85
Kevin Stark <kstark734@gmail.com>
2023-03-05 19:44:52 +0000

    Add user reducer to store

--- src/features/users/userSlice.js
+++ src/features/users/userSlice.js
@@ -1,6 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const initalState = {}
+const initalState = [
+  { id: '0', name: 'Tianna Jenkins' },
+  { id: '1', name: 'Kevin Grant' },
+  { id: '2', name: 'Madison Price' },
+]
 
 const userSlice = createSlice({
   name: 'users',
