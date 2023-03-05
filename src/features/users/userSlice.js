cdb53273f17f249902164ad9d8189be5c8590a00
Kevin Stark <kstark734@gmail.com>
2023-03-05 19:00:11 +0000

    Create empty user slice

---
+++ src/features/users/userSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initalState = {}
+
+const userSlice = createSlice({
+  name: 'users',
+  initalState,
+  reducers: {},
+})
+
+export default userSlice.reducer
