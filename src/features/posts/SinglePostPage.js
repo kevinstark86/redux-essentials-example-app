a052776bf21ac5bab744176be323fae924cc0e7a
Kevin Stark <kstark734@gmail.com>
2023-03-06 16:44:12 +0000

    Add reactions

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -2,6 +2,7 @@ import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { PostAuthor } from './PostAuthor'
 import { TimeAgo } from './TimeAgo'
+import { ReactionButtons } from './ReactionButton'
 
 export const SinglePostPage = ({ match }) => {
   const { postId } = match.params
@@ -24,6 +25,7 @@ export const SinglePostPage = ({ match }) => {
         <h2>{post.title}</h2>
         <PostAuthor userId={post.user} />
         <TimeAgo timeStamp={post.date} />
+        <ReactionButtons post={post} />
         <p className="post-content">{post.content}</p>
         <Link to={`/editPost/${post.id}`}>Edit Post</Link>
       </article>
