23fbd824c3f41d9056f442bac11f3810fe3c5732
Kevin Stark <kstark734@gmail.com>
2023-03-07 15:21:40 +0000

    create asyncThunk to fetch posts and refactor

--- src/features/posts/PostLists.js
+++ src/features/posts/PostLists.js
@@ -1,35 +1,60 @@
-import { useSelector } from 'react-redux'
+import React, { useEffect } from 'react'
+import { Spinner } from '../../components/Spinner'
+
+import { useSelector, useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { selectAllPosts } from './postSlice'
+import { selectAllPosts, fetchPosts } from './postSlice'
 import { PostAuthor } from './PostAuthor'
 import { TimeAgo } from './TimeAgo'
 import { ReactionButtons } from './ReactionButton'
 
-const PostLists = () => {
-  const posts = useSelector(selectAllPosts)
-  const orderedPosts = posts
-    .slice()
-    .sort((a, b) => b.date.localeCompare(a.date))
+const PostExcerpt = ({ post }) => {
+  return (
+    <article className="post-excerpt" key={post.id}>
+      <h3>{post.title}</h3>
+      <PostAuthor userId={post.user} />
+      <TimeAgo timeStamp={post.date} />
+      <ReactionButtons post={post} />
+      <p className="post-content">{post.content.substring(0, 100)}</p>
+      <Link to={`/posts/${post.id}`} className="button muted-button">
+        View Post
+      </Link>
+    </article>
+  )
+}
 
-  const renderedPosts = orderedPosts.map((item) => {
-    return (
-      <article className="post-excerpt" key={item.id}>
-        <h3>{item.title}</h3>
-        <PostAuthor userId={item.user} />
-        <TimeAgo timeStamp={item.date} />
-        <ReactionButtons post={item} />
-        <p className="post-content">{item.content.substring(0, 100)}</p>
-        <Link to={`/posts/${item.id}`} className="button muted-button">
-          View Post
-        </Link>
-      </article>
-    )
-  })
+const PostLists = () => {
+  const dispatch = useDispatch()
+  const posts = useSelector(selectAllPosts)
+  const postStatus = useSelector((state) => state.posts.status)
+  const error = useSelector((state) => state.posts.error)
+
+  useEffect(() => {
+    if (postStatus === 'idle') {
+      dispatch(fetchPosts())
+    }
+  }, [postStatus, dispatch])
+
+  let content
+
+  if (postStatus === 'loading') {
+    content = <Spinner text="Loading..." />
+  } else if (postStatus === 'succeeded') {
+    const orderedPosts = posts
+      .slice()
+      .sort((a, b) => b.date.localeCompare(a.date))
+
+    content = orderedPosts.map((post) => {
+      return <PostExcerpt key={post.id} post={post} />
+    })
+  } else if (postStatus === 'error') {
+    content = <div>{error}</div>
+  }
 
   return (
     <section className="posts-lists">
       <h2>Posts</h2>
-      {renderedPosts}
+      {content}
     </section>
   )
 }
--- src/features/posts/postSlice.js
+++ src/features/posts/postSlice.js
@@ -1,11 +1,16 @@
-import { createSlice, nanoid } from '@reduxjs/toolkit'
-import { sub } from 'date-fns'
+import { createSlice, nanoid, createAsyncThunk } from '@reduxjs/toolkit'
+import { client } from '../../api/client'
 
 const initialState = {
   posts: [],
   status: 'idle',
   error: null,
 }
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+  const response = await client.get('/fakeApi/posts')
+  return response.data
+})
 
 const postSlice = createSlice({
   name: 'posts',
@@ -47,6 +52,19 @@ const postSlice = createSlice({
         existingPost.reactions[reaction]++
       }
     },
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchPosts.pending, (state) => {
+        state.status = 'loading'
+      })
+      .addCase(fetchPosts.fulfilled, (state, action) => {
+        state.status = 'succeeded'
+        state.posts = state.posts.concat(action.payload)
+      })
+      .addCase(fetchPosts.rejected, (state, action) => {
+        state.error = action.error.message
+      })
   },
 })
 
